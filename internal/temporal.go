d96388a67d1bec66e8ad9f2a69a2f007178aaf06
Dylan Turnbull <dylanturn@gmail.com>
2022-01-13 08:52:21 -0600

    make sure that only the resource_info workflow is triggered

--- internal/temporal.go
+++ internal/temporal.go
@@ -6,9 +6,7 @@ import (
 	"go.temporal.io/sdk/client"
 	"log"
 	"paul/internal/util"
-	resource_count "paul/internal/workflows/resource-count"
 	resource_info "paul/internal/workflows/resource-info"
-	resource_status "paul/internal/workflows/resource-status"
 )
 
 func StartTemporal() client.Client {
@@ -29,16 +27,18 @@ func ExecuteWorkflow(temporalClient client.Client, intentAction string, intentPa
 	var workExec client.WorkflowRun
 	var executionResponse string
 
-	switch intentAction {
-	case "workflow.resource_info":
-		workExec = resource_info.ExecuteWorkflow(temporalClient, intentParameters)
-	case "workflow.resource_count":
-		workExec = resource_count.ExecuteWorkflow(temporalClient, intentParameters)
-	case "workflow.resource_status":
-		workExec = resource_status.ExecuteWorkflow(temporalClient, intentParameters)
-	default:
-		panic("can't find workflow: " + intentAction)
-	}
+	workExec = resource_info.ExecuteWorkflow(temporalClient, intentParameters)
+
+	//	switch intentAction {
+	//	case "workflow.resource_info":
+	//		workExec = resource_info.ExecuteWorkflow(temporalClient, intentParameters)
+	//	case "workflow.resource_count":
+	//		workExec = resource_count.ExecuteWorkflow(temporalClient, intentParameters)
+	//	case "workflow.resource_status":
+	//		workExec = resource_status.ExecuteWorkflow(temporalClient, intentParameters)
+	//	default:
+	//		panic("can't find workflow: " + intentAction)
+	//	}
 
 	log.Println("Started workflow", "WorkflowID", workExec.GetID(), "RunID", workExec.GetRunID())
 	err := workExec.Get(context.Background(), &executionResponse)
